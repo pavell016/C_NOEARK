bc700d37e63f05f05808f474a7a585881f653f58
pavell <pavellupan29@gmail.com>
2025-01-16 09:12:01 +0100

    socols

---
+++ C#/socols/projecte_exemple/Socols/Socols/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace server {
+    class programa {
+        static void Main(String[] args) {
+            // (definicio socket)
+            IPAddress MyIPAddress;
+            MyIPAddress = IPAddress.Parse("127.0.0.1");
+            int MyPort = 11000;
+
+            //servidor
+            TcpListener server = new TcpListener(MyIPAddress, MyPort);
+            server.Start();
+            Console.WriteLine("Server Iniciat");
+
+            TcpClient Client = server.AcceptTcpClient();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/socols/projecte_exemple/Socols/socolClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace client
+{
+    class programa
+    {
+        static void Main(String[] args)
+        {
+            // (definicio socket)
+            IPAddress MyIPAddress;
+            MyIPAddress = IPAddress.Parse("127.0.0.1");
+            int MyPort = 11000;
+
+            //servidor
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect(MyIPAddress, MyPort);
+
+            Console.WriteLine("Connexio establerta");
+            Console.ReadLine();
+        }
+    }
+}
